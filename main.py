5aea266149c3b824dbfdeb95b9e522c1ef562e4b
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-08-27 21:55:07 +0300

    logo + fromater

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes import route
 
 app = FastAPI()
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # include the router from route.py
 app.include_router(route.router)
